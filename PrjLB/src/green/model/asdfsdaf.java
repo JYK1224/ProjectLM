75c9871ad011995eec5dd63028852f7375584a0b
GGG <GGG@DESKTOP-8N3GG4O>
2023-02-17 13:16:41 +0900

    추가

---
+++ PrjLB/src/green/model/asdfsdaf.java
@@ -0,0 +1,5 @@
+package green.model;
+
+public class asdfsdaf {
+
+}
---
+++ PrjLB/src/green/view/dsadf.java
@@ -0,0 +1,10 @@
+package green.view;
+
+public class dsadf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
